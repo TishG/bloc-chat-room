1f78c1ec083fbea178b40ab36c0376e05144b1ce
Tish <latisha.griffiths@gmail.com>
2018-08-23 15:22:47 -0500

    assignment - list messages: completed

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -13,7 +13,6 @@ class MessageList extends Component {
       this.messagesRef.on("child_added", snapshot => {
         const message = snapshot.val();
         message.key = snapshot.key;
-        console.log(snapshot.key)
         this.setState({ messages: this.state.messages.concat( message ) })
       });
     }
@@ -22,14 +21,14 @@ class MessageList extends Component {
     render() {
       return (
         <main className="message-room">
-        <h2>{this.props.activeRoom ? this.props.activeRoom.name : " " }</h2>
+        <h2>{this.props.activeRoom ? this.props.activeRoom.name : " "}</h2>
           <ul className="message-list">
             {
               this.state.messages.map( (message, index) =>
                 { return ( <li
                         key={index}
                         className="message">
-                        {message.roomId === room.key ? message.content: " "}:{message.roomId}
+                        {message.roomId === this.props.activeRoom.key ? message.content: " "}
                        </li>
                      )
                 })
